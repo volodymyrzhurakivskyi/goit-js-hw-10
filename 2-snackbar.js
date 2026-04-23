af975c86031546d593dfd4f9aa438659d9317880
volodymyrzhurakivskyi <zhurakivskyivolodymyr@gmail.com>
2026-04-23 17:00:43 +0000

    Deploying to gh-pages from @ volodymyrzhurakivskyi/goit-js-hw-10@5da044cd035807dea8410ea126de8c1bc9681bd8 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/styles-DFBWcUTN.js";import{i as m}from"./assets/vendor-BbbuE1sJ.js";const s=document.querySelector(".form");s.addEventListener("submit",e=>{e.preventDefault();const i=Number(s.elements.delay.value),o=s.elements.state.value;r(i,o).then(t=>{m.success({message:`✅ Fulfilled promise in ${t}ms`,position:"topRight",timeout:5e3})}).catch(t=>{m.error({message:`❌ Rejected promise in ${t}ms`,position:"topRight",timeout:5e3})}),s.reset()});function r(e,i){return new Promise((o,t)=>{setTimeout(()=>{i==="fulfilled"?o(e):t(e)},e)})}
+//# sourceMappingURL=2-snackbar.js.map
